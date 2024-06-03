a839d67a684f78cf53746085bb2bf0b830b5a9b5
Pista agario <freeaccforu98@gmail.com>
2024-06-03 18:52:15 +0300

    update

--- api/dbh.inc.php
+++ api/dbh.inc.php
@@ -1,8 +1,14 @@
 <?php
 
-$dsn = $_ENV["DSN"];
-$dbusername = $_ENV["DBUSERNAME"];
-$dbpassword = $_ENV["DBPSWD"];
+// $dsn = $_ENV["DSN"];
+// $dbusername = $_ENV["DBUSERNAME"];
+// $dbpassword = $_ENV["DBPSWD"];
+
+$dsn = "mysql:host=sql8.freemysqlhosting.netport=3306;dbname=sql8711485";
+$dbusername = "sql8711485";
+$dbpassword = "rbPhrsUKTn";
+
+
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
